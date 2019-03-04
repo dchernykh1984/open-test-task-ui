40b2d7fac9b2dadbd0eb0ae6502640c22c54e9b5
Denis Chernykh <denis_chernykh@epam.com>
2019-03-04 13:33:19 +0300

    refactor api test - replace 2 scenarios by 1 with dataprovider

--- src/test/java/com/epam/bko/at/testtask/GitHubApi.java
+++ src/test/java/com/epam/bko/at/testtask/GitHubApi.java
@@ -3,13 +3,22 @@ package com.epam.bko.at.testtask;
 import com.epam.bko.at.testtask.helpers.PropertiesReader;
 import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-public class GitHubApiSuccess {
+public class GitHubApi {
     ValidatableResponse response;
+
+    @DataProvider
+    public Object[][] parametersForValidation() {
+        return new Object[][]{
+                {"login", PropertiesReader.getProperty("login")},
+                {"company", PropertiesReader.getProperty("company")}
+        };
+    }
 
     @BeforeClass(description = "Make request to https://api.github.com/users/defunkt")
     public void makeRequest() {
@@ -17,8 +26,8 @@ public class GitHubApiSuccess {
                 then().statusCode(200);
     }
 
-    @Test(description = "Check login contains property login")
-    public void checkLogin() {
-        response.body("login", equalTo(PropertiesReader.getProperty("login")));
+    @Test(description = "Check login contains property login", dataProvider = "parametersForValidation")
+    public void checkLogin(String parameterName, String parameterValue) {
+        response.body(parameterName, equalTo(parameterValue));
     }
 }
